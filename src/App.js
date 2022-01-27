fd532ac92f94acf14ad6148452d903e7239ca488
EmilianoMaestromey <eemrp97@gmail.com>
2022-01-26 23:45:36 -0300

    Nombre de mi proyecto

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Cafetería
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Precios agradables para tu bolsillo.
         </p>
         <a
           className="App-link"

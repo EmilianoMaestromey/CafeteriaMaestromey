767c1e93748ccd82226622047934ae4e895dd895
EmilianoMaestromey <eemrp97@gmail.com>
2022-02-01 20:33:57 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Cafetería
-        </h1>
-        <p>
-          Precios agradables para tu bolsillo.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = () => {
+    return (
+        <>
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Coffee & Beer</a>
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-outline-light">Categoría 1</button>
+                        <button type="button" class="btn btn-outline-light">Categoría 2</button>
+                        <button type="button" class="btn btn-outline-light">Categoría 3</button>
+                        <button type="button" class="btn btn-outline-light">Categoría 4</button>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default NavBar;
